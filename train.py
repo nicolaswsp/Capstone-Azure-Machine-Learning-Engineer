fb599f57bb786967c1b86006fdc38f52e72d5025
Nícolas Pauli <nicolaswsp@gmail.com>
2021-04-16 22:09:58 +0000

    Last updates

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-import sklearn
 from sklearn.ensemble import RandomForestClassifier
 import argparse
 import os
@@ -11,12 +10,19 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 import requests
-import zipfile
+from zipfile import ZipFile
+from io import BytesIO
 from azureml.core import Workspace, Dataset
 
-ws = Workspace.from_config()
-dataset = Dataset.get_by_name(workspace=ws, name='Banking_Transactions')
-transactions_df = dataset.to_pandas_dataframe()
+subscription_id = '510b94ba-e453-4417-988b-fbdc37b55ca7'
+resource_group = 'aml-quickstarts-142871'
+workspace_name = 'quick-starts-ws-142871'
+
+url = 'https://s3.amazonaws.com/video.udacity-data.com/topher/2019/January/5c534768_creditcardfraud/creditcardfraud.zip'
+filename = requests.get(url).content
+zf = ZipFile(BytesIO(filename), 'r')
+zf.extractall()
+transactions_df = pd.read_csv('./creditcard.csv')
 
 # create the features dataframe
 x = transactions_df.iloc[:, :-1]
@@ -34,9 +40,9 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--n_estimators', type=int, default=100, help="The number of trees in the forest")
-    parser.add_argument('--max_depth', type=int, default=None, help="The maximum depth of the tree")
+    parser.add_argument('--max_depth', type=int, default=8, help="The maximum depth of the tree")
     parser.add_argument('--min_samples_split', type=int, default=2, help="The minimum number of samples required to split an internal node")
-    parser.add_argument('--max_features', type=int, default='auto', help="The number of features to consider when looking for the best split")
+    parser.add_argument('--max_features', type=int, default=15, help="The number of features to consider when looking for the best split")
 
     args = parser.parse_args()
 
