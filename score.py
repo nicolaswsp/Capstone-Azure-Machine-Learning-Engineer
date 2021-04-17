c4c864e32718afe0dbcbf45d909b56b241fa55d5
Nícolas Pauli <nicolaswsp@gmail.com>
2021-04-17 17:28:25 +0000

    Last u

---
+++ score.py
@@ -0,0 +1,22 @@
+# source: https://docs.microsoft.com/en-us/azure/machine-learning/how-to-deploy-and-where?tabs=python
+import pickle
+import json
+import pandas as pd
+from sklearn.externals import joblib
+from azureml.core.model import Model
+
+def init():
+    global model
+    
+    model_path = Model.get_model_path('best_automl_model')
+    model = joblib.load(model_path)
+
+
+def run(data):
+    try:
+        data = pd.DataFrame(json.loads(data)['data'])
+        result = model.predict(data)
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+        return error
