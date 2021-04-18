54d7adeb24f3f0b831889bf6c42182bf7e52837d
Nícolas Pauli <nicolaswsp@gmail.com>
2021-04-18 19:51:56 +0000

    Final changes

--- score.py
+++ score.py
@@ -14,7 +14,7 @@ def init():
 
 def run(data):
     try:
-        data = json.loads(data)
+        data = pd.DataFrame(json.loads(data)['data'])
         result = model.predict(data)
         return result.tolist()
     except Exception as e:
